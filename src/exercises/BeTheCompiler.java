c8b6dac68d7947c3697cdb3a899c5ebbaf9d9fd3
irah rosete <irah.rosete@outlook.com>
2021-11-26 00:47:18 +1100

    complete bethecompiler class

---
+++ src/exercises/BeTheCompiler.java
@@ -0,0 +1,23 @@
+package exercises;
+
+public class BeTheCompiler {
+    public static void main (String [] args) {
+        int x = 1;
+        while ( x < 10 ) {
+            if ( x > 3) {
+                System.out.println("big x");
+            }
+        x = x + 2;
+        }
+    }
+
+    public static void five (String [] args) {
+        int x = 5;
+        while ( x > 1 ) {
+            x = x - 1;
+            if ( x < 3) {
+                System.out.println("small x");
+            }
+        }
+    }
+}
