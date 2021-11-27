26337908d0ca160dcbea585468dd19d10db26a5e
irah rosete <irah.rosete@outlook.com>
2021-11-27 15:24:46 +1100

    complete drumkit

---
+++ src/chapter2/DrumKit.java
@@ -0,0 +1,27 @@
+package chapter2;
+
+public class DrumKit {
+    boolean topHat = true;
+    boolean snare = true;
+
+    void playTopHat() {
+        System.out.println("ding ding da-ding");
+    }
+
+    void playSnare() {
+        System.out.println("bang bang na-bang");
+    }
+}
+
+class DrumKitTestDrive {
+    public static void main(String[] args) {
+        DrumKit d = new DrumKit();
+        d.playTopHat();
+        d.playSnare();
+
+        d.snare = false;
+        if (d.snare) {
+            d.playSnare();
+        }
+    }
+}
